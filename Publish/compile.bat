ee903e4e552d6fcbbff547b71f55378362274d8d
babyrjw <babyrjw@163.com>
2015-10-20 13:23:10 +0800

    modify apktool 2.0.0 to 2.0.2

--- Publish/compile.bat
+++ Publish/compile.bat
@@ -1,2 +1,2 @@
-java -jar apktool_2.0.0.jar b %1 -f -o %2
+java -jar apktool_2.0.2.jar b %1 -f -o %2
 exit
--- Publish/decompile.bat
+++ Publish/decompile.bat
@@ -1,2 +1,2 @@
-java -jar apktool_2.0.0.jar d %1 -f -o %2
+java -jar apktool_2.0.2.jar d %1 -f -o %2
 exit
